9372df846d9731569e1dadefa6f861553a7087d9
gabih94 <gabimromualdo94@gmail.com>
2024-02-28 12:37:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App-body">
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,20 @@
+
+const FirstComponent = () => {
+
+    return (
+        <div className="App">
+        <h1> Hello World!</h1>
+        <div className="lista">
+          <h3>Animais</h3>
+          <ul>
+            <li>Gato</li>
+            <li>Cachorro</li>
+            <li>Hamster</li>
+            <li>Peixe</li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+export default FirstComponent;
